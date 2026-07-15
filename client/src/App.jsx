307902b158f968c30a6181b77f42c5d211dc8aab
Anish Chand <anishchand.data@gmail.com>
2026-07-15 14:17:54 +0530

    server url add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import InterviewHistory from "./pages/InterviewHistory";
 import Pricing from "./pages/Pricing";
 import InterviewReport from './pages/InterviewReport';
 
-export const ServerUrl = "http://localhost:5000";
+export const ServerUrl = "https://smartmockai.onrender.com";
 
 const App = () => {
   const dispatch = useDispatch();
